f5eca64bdbab869f1a51fa5f63a5a8f037a25f24
scxrpvo <maker_owlets0k@icloud.com>
2026-06-29 19:28:30 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+<defs><linearGradient id="g" x1="0" x2="1"><stop stop-color="#0ea5e9"/><stop offset="1" stop-color="#22d3ee"/></linearGradient></defs>
+<rect width="64" height="64" rx="12" fill="#0b1220"/>
+<text x="16" y="44" font-family="Arial, Helvetica, sans-serif" font-size="28" font-weight="700" fill="url(#g)">TR</text>
+</svg>
